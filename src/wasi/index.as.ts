c5b7eb7db198ad5088748815ebd08650ba8d7170
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-04 00:48:18 -0500

    feat: add a wasi wrapper

---
+++ src/wasi/index.as.ts
@@ -0,0 +1,26 @@
+import "wasi"
+import { Console, CommandLine } from "as-wasi"
+
+import { minify } from "../wasm/index.as"
+// import { readJsonFile } from "./lib.as"
+
+function main(): void {
+  const commandLine = new CommandLine()
+  const args = commandLine.all()
+
+  if (args.length < 2) {
+    throw new Error("The number of arguments passed to as-jsonminify is not sufficient")
+  }
+
+  // TODO fails to read the file
+  // const filePath = args[1]
+  // const jsonString = readJsonFile(filePath)
+
+  const jsonString = args[1]
+
+  const minifiedJsonString = minify(jsonString)
+
+  Console.log(minifiedJsonString)
+}
+
+main()
