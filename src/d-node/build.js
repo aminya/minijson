1446ad79a780461cbbdf9ccdaba98231adddd935
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-04 21:27:54 -0500

    fix: strip binaries

--- src/d-node/build.js
+++ src/d-node/build.js
@@ -1,5 +1,6 @@
 const { join, dirname } = require("path")
 const { renameSync, mkdirSync } = require("fs")
+const { execFileSync } = require("child_process")
 
 const exeExtention = process.platform === "win32" ? ".exe" : ""
 const binName = `minijson${exeExtention}`
@@ -9,6 +10,14 @@ const minijsonSource = join(distFolder, binName)
 
 const distPlatformFolder = join(distFolder, `${process.platform}-${process.arch}`)
 mkdirSync(distPlatformFolder, { recursive: true })
+
+function stripBin(file) {
+  if (process.platform === "win32") {
+    return
+  }
+  return execFileSync(process.env.STRIP || "strip", [file, process.platform === "darwin" ? "-Sx" : "--strip-all"])
+}
+stripBin(minijsonSource)
 
 const minijsonDist = join(distPlatformFolder, binName)
 
