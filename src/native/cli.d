d70abce1870d77ea36363f7eff54137510f341b0
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-06 20:49:16 -0500

    feat: add minify json string to the cli

--- src/native/cli.d
+++ src/native/cli.d
@@ -1,26 +1,42 @@
 module minijson.cli;
 
-import minijson.lib : minifyFiles;
+import minijson.lib : minifyFiles, minifyString;
 
 import std.getopt : getopt, defaultGetoptPrinter, GetoptResult;
 
 /** Print help */
 void printHelp(GetoptResult optResult) @trusted
 {
-  return defaultGetoptPrinter("Usage: minify json files.\nminijson --file file1.json --file file2.json", optResult
-      .options);
+  return defaultGetoptPrinter(`Usage: minify json
+    minijson --file file1.json --file file2.json
+    minijson --string '{"some_json": "string_here"}'
+  `, optResult.options);
 }
 
 void main(string[] args) @trusted
 {
   string[] files;
+  string jsonString;
 
-  auto optResult = getopt(args, "file", "the json file to minify", &files);
+  auto optResult = getopt(args, "file", "an array of files to minify", &files, "string",
+      "a json string to minify", &jsonString);
 
-  if (optResult.helpWanted || !files)
+  if (optResult.helpWanted || (!files && !jsonString))
   {
     return printHelp(optResult);
   }
 
-  minifyFiles(files);
+  // minify the given files
+  if (files)
+  {
+    minifyFiles(files);
+  }
+
+  // minify the given string and print to stdout
+  if (jsonString)
+  {
+    import std : writeln;
+
+    writeln(minifyString(jsonString));
+  }
 }
