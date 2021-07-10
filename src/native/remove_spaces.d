3ef75cd3c33ba9913f2e6d8c88fcc9dae542f955
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-10 03:23:30 -0500

    fix: use fromStringz in remove_spaces

--- src/native/remove_spaces.d
+++ src/native/remove_spaces.d
@@ -6,8 +6,8 @@ import std : fromStringz, toStringz;
 /** Removes spaces from the original string */
 string remove_spaces(string str) nothrow
 {
-  char* cstr = cast(char*) toStringz(str);
+  auto cstr = cast(char*) toStringz(str.dup);
   const length = str.length;
   const keepingPos = sse4_despace_branchless_u4(cstr, length);
-  return str[0 .. keepingPos];
+  return (cast(string) fromStringz(cstr))[0 .. keepingPos];
 }
