4438d339c6e53ead41945fff166b531f5e32ca6d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-10 22:55:15 -0500

    chore: add minifyString benchmark

--- benchmark/benchmark.d
+++ benchmark/benchmark.d
@@ -1,17 +1,44 @@
 module minijson.benchmark;
 
-import minijson.lib: minifyFiles;
+import minijson.lib : minifyFiles, minifyString;
 
-import std.datetime.stopwatch: benchmark;
-import std: dirEntries, array, SpanMode, writeln, map;
+import std : dirEntries, array, SpanMode, writeln, map, readText, parallel, iota;
+import std.datetime.stopwatch : benchmark, StopWatch, AutoStart, Duration;
 
-void main() {
+void main()
+{
   const string[] files = dirEntries("./test/fixtures/standard", SpanMode.shallow).map!(entry => entry.name).array();
 
-  void minifyFilesCaller() {
-    minifyFiles(files);
+  string[] filesContent;
+  foreach (file; files.parallel())
+  {
+    filesContent ~= readText(file);
   }
-  auto result = benchmark!(minifyFilesCaller)(1);
 
+  Duration result;
+  auto sw = StopWatch(AutoStart.yes);
+
+  writeln("Benchmark minifyFiles");
+  sw.reset();
+
+  minifyFiles(files);
+
+  result = sw.peek();
   writeln(result);
+
+  writeln("Benchmark minifyString");
+  const repeat = 20;
+  string tmp;
+
+  sw.reset();
+  foreach (_; iota(repeat))
+  {
+    foreach (fileContent; filesContent)
+    {
+      tmp = minifyString(fileContent);
+    }
+  }
+  result = sw.peek();
+
+  writeln(result / repeat);
 }
