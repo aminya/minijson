3aa99167c9770348834683f4f5940fcf74c147b8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-10 23:06:40 -0500

    chore: add options to native benchmark

--- benchmark/benchmark.d
+++ benchmark/benchmark.d
@@ -2,11 +2,16 @@ module minijson.benchmark;
 
 import minijson.lib : minifyFiles, minifyString;
 
-import std : dirEntries, array, SpanMode, writeln, map, readText, parallel, iota;
+import std : dirEntries, array, SpanMode, writeln, map, readText, parallel, iota, getopt;
 import std.datetime.stopwatch : benchmark, StopWatch, AutoStart, Duration;
 
-void main()
+void main(string[] args)
 {
+  bool benchmarkMinifyFiles = false;
+  bool benchmarkMinifyString = true;
+
+  getopt(args, "benchmark-minifyFiles", &benchmarkMinifyFiles, "benchmark-minifyString", &benchmarkMinifyString);
+
   const string[] files = dirEntries("./test/fixtures/standard", SpanMode.shallow).map!(entry => entry.name).array();
 
   string[] filesContent;
@@ -18,27 +23,34 @@ void main()
   Duration result;
   auto sw = StopWatch(AutoStart.yes);
 
-  writeln("Benchmark minifyFiles");
-  sw.reset();
-
-  minifyFiles(files);
-
-  result = sw.peek();
-  writeln(result);
-
-  writeln("Benchmark minifyString");
-  const repeat = 20;
-  string tmp;
-
-  sw.reset();
-  foreach (_; iota(repeat))
+  if (benchmarkMinifyFiles)
   {
-    foreach (fileContent; filesContent)
-    {
-      tmp = minifyString(fileContent);
-    }
-  }
-  result = sw.peek();
+    writeln("Benchmark minifyFiles");
+    sw.reset();
 
-  writeln(result / repeat);
+    minifyFiles(files);
+
+    result = sw.peek();
+    writeln(result);
+  }
+
+  if (benchmarkMinifyString)
+  {
+    writeln("Benchmark minifyString");
+    const repeat = 120;
+    auto repeater = iota(repeat);
+    string tmp;
+
+    sw.reset();
+    foreach (_; repeater)
+    {
+      foreach (fileContent; filesContent)
+      {
+        tmp = minifyString(fileContent);
+      }
+    }
+    result = sw.peek();
+
+    writeln(result / repeat);
+  }
 }
